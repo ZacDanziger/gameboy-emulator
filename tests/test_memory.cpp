bc8e1302d66fe1bcc4eb02da9e98d5b6484f4fa6
ZacDanziger <zacdanziger@gmail.com>
2025-10-30 15:51:31 -0500

    Working on getting GTests working

--- tests/test_memory.cpp
+++ tests/test_memory.cpp
@@ -2,12 +2,51 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-TEST(MemoryTest, DefaultConstructor) {
+#include <cstdlib>
+#include <fstream>
+#include <array>
+
+
+TEST(MemoryTest, LoadROM) {
+    Memory mem;
+
+    std::srand(std::time(NULL));
+    std::ofstream out;
+    out.open("numbers.txt", std::fstream::binary);
+    if (!out.is_open()) {
+        throw std::runtime_error("Cannot open file");
+    }
+
+    std::array<uint8_t, ROM_BANK_SIZE> nums = {0};
+
+    for (int i = 0; i < ROM_BANK_SIZE; i++) {
+        nums[i] = std::rand() % 0xFFFF;
+    }
+
+    for (uint8_t num : nums) {
+        out << num << '\n';
+    }
+
+    out.close();
+
+    EXPECT_NO_THROW(
+        mem.load_rom("numbers.txt");
+    );
+
+    
+}
+
+TEST(MemoryTest, HandlesRead) {
 
 }
 
-int main() {
-    test_memory_constructor();
+TEST(MemoryTest, HandlesWrite) {
 
-    return 0;
+}
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
